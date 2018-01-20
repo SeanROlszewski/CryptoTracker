b5c99b2e3bc516395a567f0e7654543169fa4fa2
SeanROlszewski <sean.r.olszewski@gmail.com>
2018-01-20 13:47:06 -0500

    work in progress on hitting the crypto compare api

--- CryptoTrackerTests/CryptoTrackerTests.swift
+++ CryptoTrackerTests/CryptoTrackerTests.swift
@@ -33,3 +33,58 @@ class CryptoCompareResponseTests: XCTestCase {
         XCTAssertEqual(response.data[2].time, 1516320000)
     }
 }
+
+enum CryptoCurrency {
+    case xrp
+}
+
+class CryptoCompareClient {
+    
+    let session = URLSession(configuration: .default)
+    
+    private func url(for currency: CryptoCurrency, from startDate: Date, to endDate: Date) -> URL {
+        return URL(string: "https://google.com")!
+    }
+    
+    func getHistoricalData(forCurrency currency: CryptoCurrency, from startDate: Date,  to endDate: Date, using completionHandler: (CryptoCompareResponse)->()) {
+        let task = session.dataTask(with: url(for: currency, from: startDate,  to: endDate))
+        
+    }
+}
+
+class CryptoCompareClientTests: XCTestCase {
+    func test_retrievingHistoricalData() {
+        let retrievalExpectation = expectation(description: "retrieves historical data from CryptoCompare for a given crypto currency")
+        
+        let client = CryptoCompareClient()
+        
+        let startDate = DateComponents(calendar: .current,
+                                       timeZone: TimeZone(identifier: "GMT"),
+                                       year: 2017,
+                                       month: 12,
+                                       day: 1).date!
+        
+        let endDate = DateComponents(calendar: .current,
+                                     timeZone: TimeZone(identifier: "GMT"),
+                                     year: 2017,
+                                     month: 12,
+                                     day: 3).date!
+        
+        client.getHistoricalData(forCurrency: .xrp,
+                                 from: startDate,
+                                 to: endDate) { response in
+                                    
+                                    guard response.data.count == 3 else {
+                                        XCTFail("Expected there to be 3 data points, but got \(response.data.count)")
+                                        return
+                                    }
+                                    
+                                    retrievalExpectation.fulfill()
+        }
+        
+        wait(for: [retrievalExpectation], timeout: 5.0)
+        
+    }
+    
+}
+
